9f48895ea64c0c33159ff2d69ab566f59aac4305
Erik-Jan van de Wal <ejvandewal@gmail.com>
2018-05-24 20:53:39 +0200

    Developed fork() process and parent termination logic.

--- main.c
+++ main.c
@@ -1,11 +1,51 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int main(int argc, char **argv) {
-  
-    char * paramsList[] = { "/usr/bin/php", "-c", "/usr/bin/php /home/erikjan/PHPProjects/test/test.php", NULL };
-    execv("/bin/bash", paramsList); 
+
+    printf("Parent process started (pid:%d)\n", getpid());
+
+    pid_t pid = fork();
+
+    if (pid < 0) { // check if fork succeeded
+        printf("fork failed!\n");
+        exit(1);
+    }
+
+    if (pid > 0) { // parent process
+        printf("Spawned child (pid:%d)\nTerminating parent process (pid:%d)\n", pid, getpid());
+        exit(0); // terminate the parent process successfully
+    }
+
+    umask(0); // un-mask inherited file modes
     
-    // if we get here it misfired
-    perror("PHP execution");
+    pid_t sid = setsid(); // set new session
+    
+    // close standard inputs, daemon process does not involve user interaction
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    FILE *fp = fopen("test.txt", "a");
+    int i = 0;
+    while(i < 10) {
+        sleep(1);
+        fprintf(fp, "TEST%d\n", i);
+        i++;
+    }
+    fclose(fp);
+    
+    return(0);
+}
+
+void executePhpScript(char* script) {
+        char * paramsList[] = { 
+            "/usr/bin/php", 
+            "-c", 
+            script, 
+            NULL 
+        };
+        execv("/bin/bash", paramsList); 
 }
